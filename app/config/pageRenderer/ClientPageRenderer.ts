1371d9abed1bb9b70f8024a9a6dd3550ed12b7e1
Anna Frankova (corvidism) <frankova@corvidism.com>
2025-03-11 10:49:42 +0100

    Add a README

---
+++ app/config/pageRenderer/ClientPageRenderer.ts
@@ -0,0 +1,1 @@
+// this should be a dummy. Does nothing.
---
+++ app/config/pageRenderer/ServerPageRenderer.ts
@@ -0,0 +1,2 @@
+// I'm doing something nasty here: turning IMA into server-side renderer only.
+// This class will render files written in a template language. ClientPageRenderer will do nothing.
